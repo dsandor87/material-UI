4341abc3ecffa5fb34f8859a6fe084b011e40bbb
Sandor Desi <d.alex.galaxy@gmail.com>
2020-06-27 17:21:30 +0100

    Material Ui-App Bar

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -3,8 +3,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import { makeStyles } from '@material-ui/core/styles'
-import { Typography } from '@material-ui/core'
 
+import logo from '../../assets/logo.svg'
 function ElevationScroll(props) {
   const { children } = props
 
@@ -20,7 +20,11 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: '3em'
+  },
+  logo: {
+    height: '7em'
   }
 }))
 
@@ -30,8 +34,8 @@ export default function Header(props) {
     <>
       <ElevationScroll>
         <AppBar position='fixed'>
-          <Toolbar>
-            <Typography variant='h3'>Arc dev</Typography>
+          <Toolbar disableGutters>
+            <img className={classes.logo} src={logo} alt='company logo'></img>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
