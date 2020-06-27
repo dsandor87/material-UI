96815a6c34b7c24f1fce842652f8cdbd8b8a15ca
Sandor Desi <d.alex.galaxy@gmail.com>
2020-06-27 17:05:38 +0100

    Material Ui-Inline Styling, Typography

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 
 function ElevationScroll(props) {
   const { children } = props
@@ -16,12 +18,24 @@ function ElevationScroll(props) {
   })
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}))
+
 export default function Header(props) {
+  const classes = useStyles()
   return (
-    <ElevationScroll>
-      <AppBar position='fixed'>
-        <Toolbar>Arc dev</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar>
+            <Typography variant='h3'>Arc dev</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   )
 }
